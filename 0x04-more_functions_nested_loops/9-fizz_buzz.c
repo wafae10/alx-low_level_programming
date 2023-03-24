f7cfa707ca569b66b8bbf53669fac2f4d12e4c4b
wafae10 <zwafae13@gmailcom>
2023-03-24 05:38:32 -0700

    updatd

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0 && i % 5 != 0)
 {
 printf(" Fizz");
-} 
+}
 else if (i % 5 == 0 && i % 3 != 0)
 {
 printf(" Buzz");
-} 
+}
 else if (i % 3 == 0 && i % 5 == 0)
 {
 printf(" FizzBuzz");
@@ -36,4 +36,3 @@ printf(" %d", i);
 printf("\n");
 return (0);
 }
-
