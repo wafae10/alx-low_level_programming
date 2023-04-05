cfb8fff3353d1725664cb9aa7a49c6b17f837d50
wafae10 <zwafae13@gmailcom>
2023-04-05 14:21:36 -0700

    updated

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**  _puts_recursion is a function like puts();
+ * @s:input
+ * Return:always 0 if success
+*/
+
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
+}
