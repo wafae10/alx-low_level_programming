5866246f256dd5abc2538801a41cb3b9e0896422
wafae10 <zwafae13@gmail.com>
2023-04-30 11:31:42 -0700

    linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,17 +2,17 @@
 /**
  * print_listint - prints the elments of the linked list.
  * @h: linked list to be printed.
- * 
+ *
  * return : the number of nodes.
 */
 size_t print_listint(const listint_t *h)
 {
-size_t num= 0;
+size_t nm= 0;
 while(h)
 {
 printf("%d\n",h->n);
-num++;
+nm++;
 h= h->next;
 }
-return(num);
+return(nm);
 }
