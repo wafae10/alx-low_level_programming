5a90a8a6bb410cf92bcea8c6f88fce0c76ee2eec
wafae10 <zwafae13@gmail.com>
2023-05-01 02:18:55 -0700

    updated

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 /**
- * pop_listint - deletes the head node
- * @head: pointer to the first element in the list
- *
- * return: the head node’s data (n)
- */
+* pop_listint - Deletes the head node of a listint_t linked list and returns the head node’s data (n)
+* @head: adress to the first element in the listint_t linked list
+*
+* return: the data inside the elements that was deleted , or return 0 if the list is empty
+*/
 int pop_listint(listint_t **head)
 {
 listint_t *tmp;
-int num;
+int nm;
 if (!head || !*head)
 return (0);
-num = (*head)->n;
+nm = (*head)->n;
 tmp = (*head)->next;
 free(*head);
 *head = tmp;
-return (num);
+return (nm);
 }
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index - returns the nth node of a listint_t linked list.
- * @head: first node in the list
- * @index: index of the node
- *
- * Return: pointer to the nth node , or NULL
- */
+* get_nodeint_at_index -  it returns the nth node of a listint_t linked list
+* @head: first node in listint_t linked list 
+* @index: index of the node to return listint_t linked list
+*
+* return: pointer to the node we looking for or NULL if node does not exist
+*/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int i = 0;
 listint_t *tmp = head;
-while (temp && i < index)
+while (tmp && i < index)
 {
-tmp = temp->next;
+tmp = tmp->next;
 i++;
 }
 return (tmp ? tmp : NULL);
