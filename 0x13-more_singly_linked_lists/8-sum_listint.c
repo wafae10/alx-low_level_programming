53e232f2727df34f46bc68a872cfdb7e363d3d68
wafae10 <zwafae13@gmail.com>
2023-05-01 04:30:03 -0700

    up to date

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,13 +3,13 @@
 * sum_listint - it returns the sum of all the data (n) of a listint_t linked list
 * @head: first node in the linked list
 *
-* Return: resulting sum , return 0 if the list is empty
+* Return: the sum of all the data, return 0 if the list is empty
 */
 int sum_listint(listint_t *head)
 {
 int sum = 0;
 listint_t *tmp = head;
-while (temp)
+while (tmp)
 {
 sum += tmp->n;
 tmp = tmp->next;
