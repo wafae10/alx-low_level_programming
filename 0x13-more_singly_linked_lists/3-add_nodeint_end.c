89e82b7fa0b8936011865823a711abf07d9c488d
wafae10 <zwafae13@gmail.com>
2023-05-01 01:58:00 -0700

    updated

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,27 +1,27 @@
 #include "lists.h"
 /**
- * add_nodeint_end - adds a node at the end of the linked list
- * @head: a pointer for the first eelement in the list
- * @n: data to drag in the new element.
- * 
- * return:the address of the new element (if success) or NULL (if it failed).
- */
+* add_nodeint_end - adds a new node at the end of a listint_t list
+* @head: a pointer to the first element in the list
+* @n: data to insert in the new element.
+*
+* Return:  the address of the new element (if success), or NULL (if failed)
+*/
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *new;
-listint_t *tmp= *head;
-new= malloc(sizeof(listint_t));
-if(!new)
-return(NULL);
-new->n=n;
-new->next=NULL;
-if (*head==NULL)
+listint_t *tmp = *head;
+new = malloc(sizeof(listint_t));
+if (!new)
+return (NULL);
+new->n = n;
+new->next = NULL;
+if (*head == NULL)
 {
-*head= new;
-return(new);
+*head = new;
+return (new);
 }
-while(tmp->next)
-tmp=tmp->next;
-tmp->next=new;
-return(new);
+while (tmp->next)
+tmp = tmp->next;
+tmp->next = new;
+return (new);
 }
