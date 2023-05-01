6a81c4f270246e44d9d192ff13a3ee7093d9ad0b
wafae10 <zwafae13@gmail.com>
2023-05-01 09:26:58 -0700

    up

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
-* get_nodeint_at_index - returns the nth node of a listint_t linked list
-* @head: first node in listint_t linked list 
-* @index: index of the node in listint_t linked list
+* get_nodeint_at_index - returns the node at the given index.
+* @head: first node in the linked list.
+* @index: index of the node.
 *
-* Return: adress to the node we looking for or NULL if empty.
+* Return: adress to the node, or NULL
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
