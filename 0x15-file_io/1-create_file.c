c6cfd6cb7422318dcf08984a32f36b639f4ce102
wafae10 <zwafae13@gmail.com>
2023-05-08 09:27:20 -0700

    file io done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* create_file - creates a file.
+* @filename: name of the file to create.
+* @text_content: Null terminated string to write to the file
+*
+* Return: 1 (if success), -1 (if failure)
+*/
+int create_file(const char *filename, char *text_content)
+{
+int or= 0;
+int w= 0;
+int lenth = 0;
+if (filename == NULL)
+return (-1);
+if (text_content != NULL)
+{
+for (lenth = 0; text_content[lenth];lenth++)
+}
+else
+or = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+w = write(or, text_content, len);
+if (or == -1 || w == -1)
+return (-1);
+close(or);
+return (1);
+}
