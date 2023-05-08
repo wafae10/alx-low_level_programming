001cbd55ab177bf6a9aa7deb8b55565cdf9db2f0
wafae10 <zwafae13@gmail.com>
2023-05-08 13:09:23 -0700

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,14 +11,14 @@ void close_file(int fd);
  */
 char *create_buffer(char *file)
 {
-char *buffer;
-buffer = malloc(sizeof(char) * 1024);
-if (buffer == NULL)
+char *buf;
+buf = malloc(sizeof(char) * 1024);
+if (buf == NULL)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 exit(99);
 }
-return (buffer);
+return (buf);
 }
 /**
  * close_file - Close file descriptors.
@@ -44,36 +44,36 @@ exit(100);
 int main(int argc, char *argv[])
 {
 int from, to, r, w;
-char *buffer;
+char *buf;
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-buffer = create_buffer(argv[2]);
+buf = create_buffer(argv[2]);
 from = open(argv[1], O_RDONLY);
-r = read(from, buffer, 1024);
+r = read(from, buf, 1024);
 to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 do 
 {
 if (from == -1 || r == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-free(buffer);
+free(buf);
 exit(98);
 }
-w = write(to, buffer, r);
+w = write(to, buf, r);
 if (to == -1 || w == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-free(buffer);
+free(buf);
 exit(99);
 }
 r = read(from, buffer, 1024);
 to = open(argv[2], O_WRONLY | O_APPEND);
 } 
 while (r > 0);
-free(buffer);
+free(buf);
 close_file(from);
 close_file(to);
 return (0);
