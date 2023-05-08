7b69a132a95e61967c44113b398a5a7f450e1bfb
wafae10 <zwafae13@gmail.com>
2023-05-08 09:31:22 -0700

    file i o done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,19 +8,18 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int or= 0;
-int w= 0;
-int lenth = 0;
+int or =0;
+int w = 0;
+int len = 0;
 if (filename == NULL)
 return (-1);
 if (text_content != NULL)
 {
-for (lenth = 0; text_content[lenth];lenth++)
+for (len = 0; text_content[len];len++)
 }
-else
 or = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-w = write(or, text_content, len);
-if (or == -1 || w == -1)
+w = write(fd, text_content, len);
+if (fd == -1 || w == -1)
 return (-1);
 close(or);
 return (1);
